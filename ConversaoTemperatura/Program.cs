ea2f21947002e2cd839fe93a49fde7da95acf371
Ari Mauri <ari.mauri.jr@gmail.com>
2023-03-05 10:13:51 -0300

    Add project files.

---
+++ ConversaoTemperatura/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double temperatura = 0.0;
+        Console.WriteLine("Digite a temperatura em graus CELSIUS");
+        temperatura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        temperatura = 1.8 * temperatura + 32;
+        Console.WriteLine("A temperatura em graus fahrenheit é " + temperatura.ToString("F2", CultureInfo.InvariantCulture));
+    }
+}
