44e1815610990819d5aa7464c41998d932e91ebb
Ari Mauri <ari.mauri.jr@gmail.com>
2023-03-05 10:15:43 -0300

    Comversão de graus celsius para fahrenheit

--- ConversaoTemperatura/Program.cs
+++ ConversaoTemperatura/Program.cs
@@ -5,6 +5,7 @@ internal class Program
     private static void Main(string[] args)
     {
         double temperatura = 0.0;
+
         Console.WriteLine("Digite a temperatura em graus CELSIUS");
         temperatura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
         temperatura = 1.8 * temperatura + 32;
